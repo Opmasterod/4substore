3a84ac4c64b97d667227e83dde72123e189e6adf
Gumas <148417921+Opmasterod@users.noreply.github.com>
2024-12-03 08:30:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,6 +31,9 @@ PORT = os.environ.get("PORT", "8080")
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://namanjain123eudhc:opmaster@cluster0.5iokvxo.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "7200")) # auto delete in seconds
+
+
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCESUB_CHANNEL", ""))
 FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", ""))
