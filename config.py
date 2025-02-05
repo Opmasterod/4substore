faa5063096ae35173e32abd86a9c2bc14ab5d052
Gumas <148417921+Opmasterod@users.noreply.github.com>
2025-02-05 05:52:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", ""))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ 🥰 {first}\n\n ɪ ᴀᴍ ᴍᴀᴛᴇʀɪᴀʟ ᴘʀᴏᴠɪᴅᴇʀ ʙᴏᴛ , ɪ ᴀᴍ ᴘʀᴏᴠɪᴅɪɴɢ ʏᴏᴜ ᴍᴀᴛᴇʀɪᴀʟ ᴡʜɪᴄʜ ʜᴇʟᴘs ʏᴏᴜ ɪɴ ʏᴏᴜʀ ᴘʀᴇᴘᴀʀᴛɪᴏɴ 🤩 » @HIDDEN_OFFICIALS</b>")
+START_MSG = os.environ.get("START_MESSAGE", "")
 try:
     ADMINS=[6376328008]
     for x in (os.environ.get("ADMINS", "5115691197 6273945163 6103092779 2005714953 5231212075 6497757690").split()):
@@ -51,7 +51,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "𝐒𝐨𝐫𝐫𝐲 {first} 𝐁𝐫𝐨/𝐒𝐢𝐬 𝐲𝐨𝐮 𝐡𝐚𝐯𝐞 𝐭𝐨 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐟𝐢𝐫𝐬𝐭 𝐭𝐨 𝐚𝐜𝐜𝐞𝐬𝐬 𝐌𝐚𝐭𝐞𝐫𝐢𝐚𝐥 \n\n ☠️𝗜 𝗵𝗮𝘃𝗲 𝘁𝗼 𝘂𝘀𝗲 𝘁𝗵𝗲𝘀𝗲 𝗰𝗵𝗮𝗻𝗻𝗲𝗹𝘀 𝗶𝗻 𝘄𝗵𝗶𝗰𝗵 𝘄𝗲 𝗮𝗹𝘄𝗮𝘆𝘀 𝘀𝘁𝗮𝘆 𝗮𝘄𝗮𝘆 𝗳𝗿𝗼𝗺 𝗰𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 🙏 \n\n𝐒𝐨 𝐩𝐥𝐞𝐚𝐬𝐞 𝐣𝐨𝐢𝐧 𝐦𝐲 𝐜𝐡𝐚𝐧𝐧𝐞𝐥𝐬 𝐟𝐢𝐫𝐬𝐭 𝐚𝐧𝐝 𝐜𝐥𝐢𝐜𝐤 𝐨𝐧 “𝐍𝐨𝐰 𝐂𝐥𝐢𝐜𝐤 𝐡𝐞𝐫𝐞” 𝐛𝐮𝐭𝐭𝐨𝐧....!")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "")
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", """<b>{previouscaption}</b>
 <b>━━━━━━━━━━━━━━━━━◇</b>
