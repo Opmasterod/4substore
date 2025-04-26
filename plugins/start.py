26505255121d6a28cb3c0dec3c270847263a89b6
Gumas <148417921+Opmasterod@users.noreply.github.com>
2025-04-26 06:27:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -201,7 +201,7 @@ async def start_command(client: Client, message: Message):
 
         # Schedule auto-deletion
         asyncio.create_task(delete_files(codeflix_msgs, client, special_copied_msg, message))
-            pass
+        return
 
     else:
         reply_markup = InlineKeyboardMarkup(
